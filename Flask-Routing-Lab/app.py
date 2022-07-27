6924c3f1e623c04df3e198710e0d04b4094f7ccd
JudehAlaaraj <109723372+JudehAlaaraj@users.noreply.github.com>
2022-07-27 18:08:59 +0300

    g

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
  
 @app.route('/')
-def index():
+def home():
     return render_template('home.html')
 @app.route('/product')
-def index2():
+def product():
     return render_template('product.html')
-@app.route('/cart.html')
-def index3():
+@app.route('/cart')
+def cart():
     return render_template('cart.html')
  
 if __name__ == '__main__':
